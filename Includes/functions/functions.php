fc7e4e165a3b41d6125a7fb293a9f28e993d32bb
Jose Polanco <jpolancog@intercorp.com.pe>
2024-09-17 11:42:09 -0500

    actualizando el tittle

--- Includes/functions/functions.php
+++ Includes/functions/functions.php
@@ -6,9 +6,9 @@
 	{
 		global $pageTitle;
 		if(isset($pageTitle))
-			echo $pageTitle." | Vincent Restaurant - Your Restaurant";
+			echo $pageTitle." | PIZZA PIZZONI";
 		else
-			echo "Vincent Restaurant | Your Restaurant";
+			echo "PIZZA PIZZONI";
 	}
 
 	/*
